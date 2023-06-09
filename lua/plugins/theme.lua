48e44b8948baf7b8a6d29320fd39d1d85add37ba
alexhayward <a46805d5451d443e97d9678283c3710a>
2023-06-09 15:57:31 +0100

    init

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,13 @@
+return {
+  -- schemes
+  { "ellisonleao/gruvbox.nvim" },
+  { "sainnhe/everforest" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
