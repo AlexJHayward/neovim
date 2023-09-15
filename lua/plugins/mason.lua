b44653ef08a24dcabdf47912af09e367fd735e60
Alex Hayward <alex.hayward@multiverse.io>
2023-09-15 14:43:34 +0100

    mason config

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,21 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shfmt",
+      "css-lsp",
+      "elixir-ls",
+      "gleam",
+      "html-lsp",
+      "json-lsp",
+      "lua-language-server",
+      "prettier",
+      "prisma-language-server",
+      "pyright",
+      "shfmt",
+      "stylua",
+      "typescript-language-server",
+    },
+  },
+}
