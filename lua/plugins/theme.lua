eb34260ded18e28919d6b1e251d799f9f00fe309
alexhayward <a46805d5451d443e97d9678283c3710a>
2023-10-31 22:28:54 +0000

    more colourschemes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,12 +2,20 @@ return {
   -- schemes
   { "ellisonleao/gruvbox.nvim" },
   { "sainnhe/everforest" },
+  { "folke/tokyonight.nvim" },
+  { "catppuccin/nvim" },
+  { "rose-pine/neovim" },
+  { "EdenEast/nightfox.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+  { "shaunsingh/nord.nvim" },
+  { "ronisbr/nano-theme.nvim" },
+  { "Lokaltog/monotone.nvim", dependencies = { "rktjmp/lush.nvim" } },
 
-  -- Configure LazyVim to load a theme
+  -- load theme on startup
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "monotone",
     },
   },
 }
